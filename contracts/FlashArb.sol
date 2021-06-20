2535a4f646a64624f2392c0de39fec6f9692523b
Ryuhei Matsuda <ryuheimat3@gmail.com>
2021-06-20 09:48:26 +0900

    Initial commit

---
+++ contracts/FlashArb.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+////////////////////////////
+/// GENERAL INSTRUCTIONS ///
+////////////////////////////
+
+// 1. AT THE TOP OF EACH CONTRACT FILE, PLEASE LIST GITHUB LINKS TO ANY AND ALL REPOS YOU BORROW FROM THAT YOU DO NOT EXPLICITLY IMPORT FROM ETC.
+// 2. PLEASE WRITE AS MUCH OR AS LITTLE CODE AS YOU THINK IS NEEDED TO COMPLETE THE TASK
+// 3. LIBRARIES AND UTILITY CONTRACTS (SUCH AS THOSE FROM OPENZEPPELIN) ARE FAIR GAME
+
+//////////////////////////////
+/// CHALLENGE INSTRUCTIONS ///
+//////////////////////////////
+
+// 1. Initiates a flash loan from Aave V2
+// 2. Uses the loaned tokens to perform an arbitrage trade between UniswapV2 and Sushiswap
+// 3. Write as much code as you deem necessary to complete the task
+// 4. Bonus: Optimize dex arbitrage trade for profit
+// 5. Please feel free to use any code/packages from the following Github Organizations and Docs:
+//      Aave Org: https://github.com/aave
+//      Aave Docs: https://docs.aave.com/developers/
+//      Uniswap Org: https://github.com/uniswap
+//      UniswapV2 Docs: https://uniswap.org/docs/v2/
+//      Sushiswap Org: https://github.com/sushiswap
+//      Sushiswap Docs: https://dev.sushi.com/
+
+contract FlashArb {
+
+}
